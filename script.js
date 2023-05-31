2c7607c30f420e96fb3fafc072cf65cb27a128ca
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-05-31 08:56:23 +0600

    push

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-async function connectToDevice() {
-  try {
-    // Request Bluetooth device
-    const device = await navigator.bluetooth.requestDevice({
-      filters: [{ services: ['<your_service_uuid>'] }]
-    });
-
-    // Connect to the device
-    const server = await device.gatt.connect();
-
-    // Get the primary service
-    const service = await server.getPrimaryService('<your_service_uuid>');
-
-    // Get the characteristic to control
-    const characteristic = await service.getCharacteristic('<your_characteristic_uuid>');
-
-    // Example: Write a value to the characteristic
-    const data = new Uint8Array([0x01, 0x02, 0x03]); // Example data
-    await characteristic.writeValue(data);
-
-    console.log('Value written to the characteristic');
-  } catch (error) {
-    console.error('Error:', error);
-  }
+function connectToDevice() {
+  navigator.bluetooth.requestDevice({acceptAllDevices: true, optionalServices: ['battery_service']})
 }
