42c6c3f248800ee0f65533bd47961b7d4f2ddc2f
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-05-31 09:47:08 +0600

    push

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 const $connectBtn = document.querySelector('.connectBtn')
 
-function connectToDevice() {
-  let device = navigator.bluetooth.requestDevice({acceptAllDevices: true}).then(res => console.log(res.gatt))
+let device;
+let uuid;
+let isConnected;
+let bluetoothCharacteristic;
 
-  let isConnected = device.gatt.connected
 
-  isConnected ? $connectBtn.setAttribute('style', 'display: none') : $connectBtn.setAttribute('style', 'display: block')
+async function connectToDevice() {
+  try {
+    let result = navigator.bluetooth.requestDevice({acceptAllDevices: true}).then(res => {
+      console.log(res.gatt)
+      uuid = res.gatt.device.id
+      isConnected = res.gatt.connected
+    })
+  
+    device = result;
+
+    const service = await server.getPrimaryService(uuid);
+
+    bluetoothCharacteristic = await service.getCharacteristic(uuid);
+
+  
+    isConnected ? $connectBtn.setAttribute('style', 'display: none') : $connectBtn.setAttribute('style', 'display: block')
+  }catch(e){
+    alert('Попробуйте еще раз!')
+  }
 }
+
+function turnOnAll() {
+  if (characteristic && characteristic.writeValue) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+    characteristic.writeValue(data)
+      .then(() => {
+        console.log('Text data sent: ' + text);
+      })
+      .catch((error) => {
+        console.error('Error sending text data:', error);
+      });
+  } else {
+    console.error('Characteristic not available for writing.');
+  }
+}
+
+
+
+console.log(uuid);
